f7d184be2c0051398249b4d30b434e32b988c61f
Roma Kulik <kulikr.js@mail.ru>
2022-09-20 21:35:49 +0300

    add first page

---
+++ dist/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+  hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+  });
+  closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+  })
+});
